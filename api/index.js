802c465a002465eed90ef25de717058dbba71a4d
unknown <nayasafwandaba@gmail.com>
2022-12-23 16:57:46 +0200

    testing

--- api/index.js
+++ api/index.js
@@ -1,14 +1,40 @@
 const express = require("express");
 const app = express();
+const cors=require("cors");
+const {mongoClient}  = require("./db");
 
-const {mongoClient}  = require("./conn");
 app.use(express.json());
 
 const port = process.env.PORT || 5000;
 
 
+mongoClient();
 
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", value="*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods","POST,PATCH,GET,DELETE");
+  next();
+});
+
+app.use(function (req, res, next) {
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
+
+app.use(cors({ origin : '*'}));
 
 app.get('/api/masterlist', async(req, res) => {
     
